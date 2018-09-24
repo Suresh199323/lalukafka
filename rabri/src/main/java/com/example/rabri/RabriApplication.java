1863b8f70a3f28e041390eaa0ba89b9137e027d5
Suresh199323 <33006772+Suresh199323@users.noreply.github.com>
2018-09-24 11:52:55 -0400

    Add files via upload

---
+++ rabri/src/main/java/com/example/rabri/RabriApplication.java
@@ -0,0 +1,12 @@
+package com.example.rabri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabriApplication.class, args);
+	}
+}
